0a54b0bb0a80922ce3898a4b613a579e5eba3507
Enigma <enigma@Enigmas-MacBook-Air.local>
2017-03-13 15:15:02 -0500

    6.1.2 in production

---
+++ if_and_or.php
@@ -0,0 +1,31 @@
+<?php
+
+$x = 0;
+$y = 5;
+$z = 10;
+
+// TODO:
+// If $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+if($x < $y and $y < $z){
+	echo "$x<$y<$z"
+}
+
+// TODO:
+// If 0 is less than $x OR $x is less than 10
+// then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
+
+// TODO:
+// repeat the if statement for $y and $z.
+
+// TODO:
+// If 0 is less than $x AND $x is less than 10
+// then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
+
+// TODO:
+// repeat the if statement for $y and $z.
+ 
+// 0 < 5 < 10
+// 0 is less than 0 OR 0 is less than 10
+// 0 is less than 5 OR 5 is less than 10
+// 0 is less than 10 OR 10 is less than 10
+// 0 is less than 5 AND 5 is less than 10
