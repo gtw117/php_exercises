bd3401125f7c99d9bf0f47127c87f631dfff28a3
Enigma <enigma@Enigmas-MacBook-Air.local>
2017-03-13 15:24:32 -0500

    completed 6.1.2 if and or

--- if_and_or.php
+++ if_and_or.php
@@ -6,26 +6,38 @@ $z = 10;
 
 // TODO:
 // If $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
-if($x < $y and $y < $z){
-	echo "$x<$y<$z"
+if(($x < $y) && ($y < $z)){
+	echo ("$x<$y<$z");
 }
 
 // TODO:
 // If 0 is less than $x OR $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
-
+if (0<$x ||  $x<10){
+	echo ("0 is less than {$x} OR {$x} is less than 10").PHP_EOL;
+}
 // TODO:
 // repeat the if statement for $y and $z.
+if (0<$y ||  $y<10){
+	echo ("0 is less than {$y} OR {$y} is less than 10").PHP_EOL;
+}
+if (0<$z ||  $z<10){
+	echo ("0 is less than {$z} OR {$z} is less than 10").PHP_EOL;
+}
 
 // TODO:
 // If 0 is less than $x AND $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
+if (0<$x && $x<10){
+		echo ("0 is less than {$x} AND {$x} is less than 10").PHP_EOL;
+}
 
 // TODO:
 // repeat the if statement for $y and $z.
+if (0<$y && $y<10){
+		echo ("0 is less than {$y} AND {$y} is less than 10").PHP_EOL;
+}
+if (0<$z && $z<10){
+		echo ("0 is less than {$z} AND {$z} is less than 10").PHP_EOL;
+}
  
-// 0 < 5 < 10
-// 0 is less than 0 OR 0 is less than 10
-// 0 is less than 5 OR 5 is less than 10
-// 0 is less than 10 OR 10 is less than 10
-// 0 is less than 5 AND 5 is less than 10
