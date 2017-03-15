5334c8049243547008691f2b57c970fce47a9666
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-15 16:45:54 -0500

    fix foreach of 2D array, lists 2D array's contents within output

--- foreach.php
+++ foreach.php
@@ -4,14 +4,20 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach($things as $type){
 	if(is_int($type)){
-		echo "{$type} is an integer.".PHP_EOL;
-	}else if(is_bool($type)){
-		echo "{$type} is of boolean nature.".PHP_EOL;
-	}else if(is_array($type)){
-		echo "{$type} is an array.".PHP_EOL;
-	}else if(is_null($type)){
-		echo "{$type} contains nothing, is null.".PHP_EOL;
-	}else if(is_string($type)){
-		echo"{$type} is a string.".PHP_EOL;
+		echo "'{$type}' is an integer.".PHP_EOL;
+	}
+	if(is_bool($type)){
+		echo "'{$type}' is of boolean nature.".PHP_EOL;
+	}
+	if(is_array($type)){
+		foreach($type as $data){
+			echo "{$data} is in an array.".PHP_EOL; ;
+		}
+	}
+	if(is_null($type)){
+		echo "contains nothing, is null.".PHP_EOL;
+	}
+	if(is_string($type)){
+		echo"'{$type}' is a string.".PHP_EOL;
 	}
 }
