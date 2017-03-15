7bb243ab062fb086e42d2c2e317faeabdcba5b29
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-15 17:01:08 -0500

    add more foreach loops for scalar and to echo all data

--- foreach.php
+++ foreach.php
@@ -1,7 +1,7 @@
 <?php
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
-
+//give data type of data:
 foreach($things as $type){
 	if(is_int($type)){
 		echo "'{$type}' is an integer.".PHP_EOL;
@@ -19,5 +19,21 @@ foreach($things as $type){
 	}
 	if(is_string($type)){
 		echo"'{$type}' is a string.".PHP_EOL;
+	}
+}
+//if scalar:
+foreach($things as $scalar){
+	echo "the following are found to be SCALAR:".PHP_EOL;
+	if(is_scalar($scalar)){
+		echo "--->{$scalar} is SCALAR.".PHP_EOL;
+	}
+}
+//echo out each of item, even if its in an array:
+foreach($things as $thingys){
+	echo "***{$thingys}***".PHP_EOL;
+	if(is_array($thingys)){
+		foreach($thingys as $arrayContents){
+			echo "***{$arrayContents}***".PHP_EOL;
+		}
 	}
 }
