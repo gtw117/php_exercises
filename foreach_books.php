f831b2780254f8b69bc4ae383b1881f350fab7a4
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-22 11:32:39 -0500

    cleaned up code

--- foreach_books.php
+++ foreach_books.php
@@ -27,17 +27,17 @@ $books = array(
 
 // //1 .need to Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
 
-	foreach ($books as $book => $title){
-		if (($title['published']) > 1950){
-			echo "--$book--".PHP_EOL;
-			foreach ($title as $key => $value){		
-					echo "$key | $value".PHP_EOL;
-			}
-		}
-	}
+
 // //2.Update your loop to only show books that were written after 1950.
 
-
+foreach ($books as $book => $title){
+	if (($title['published']) > 1950){
+		echo "--$book--".PHP_EOL;
+		foreach ($title as $key => $value){		
+				echo "$key | $value".PHP_EOL;
+		}
+	}
+}
 
 
 
