0258cad91d40ed0225969dc64c14d958868391ae
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-23 10:32:28 -0500

    First commit

---
+++ arithmetic.php
@@ -0,0 +1,23 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b .PHP_EOL;
+}
+
+function subtract($a, $b)
+{
+
+}
+
+function multiply($a, $b)
+{
+
+}
+
+function divide($a, $b)
+{
+
+}
+
+echo add(9, 3);
