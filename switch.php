ffaea67e5e2192e6f309794d34519ae5e2c82091
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-23 09:33:26 -0500

    Complete step 2, add if/else to compare to case/switch

--- switch.php
+++ switch.php
@@ -37,6 +37,25 @@ switch($dayOfWeek) {
         break;
 }
 
+echo "==========================================".PHP_EOL;
+
+// 2.Test and verify the results before proceeding by creating a matching if block. The output should be identical.
+
+if ($dayOfWeek == 1){
+	echo "monday".PHP_EOL;
+}else if ($dayOfWeek == 2){
+	echo "tuesday".PHP_EOL;
+}else if ($dayOfWeek == 3){
+	echo "wednesday".PHP_EOL;
+}else if ($dayOfWeek == 4){
+	echo "thursday".PHP_EOL;
+}else if ($dayOfWeek == 5){
+	echo "friday".PHP_EOL;
+}else if ($dayOfWeek == 6){
+	echo "saturday".PHP_EOL;
+}else if ($dayOfWeek == 7){
+	echo "sunday".PHP_EOL;
+}
 
 
 
