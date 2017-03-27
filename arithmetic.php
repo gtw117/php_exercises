6a4503509e34b3183e27381814ceaa1d81455061
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-27 12:10:25 -0500

    Displays error messages for non numeric values and values of zero

--- arithmetic.php
+++ arithmetic.php
@@ -1,10 +1,14 @@
 <?php
 
 $a = 6;
-$b = 2;
+$b = 0;
+
+if (!is_numeric($a) || !is_numeric($b)){
+	echo "ERROR: please use numbers only".PHP_EOL;
+}
 
 function add($a, $b){
-    return $a + $b .PHP_EOL;
+	return $a + $b .PHP_EOL;
 }
 
 function subtract($a, $b){
@@ -16,15 +20,29 @@ function multiply($a, $b){
 }
 
 function divide($a, $b){
-	return $a / $b .PHP_EOL;
+	if ($b <= 0){
+		echo "cannot divide by ZERO".PHP_EOL;
+	} else{
+		return $as / $b .PHP_EOL;
+	}
+	
+
 }
 
 function modulus($a, $b){
+	if ($b <= 0){
+		echo "cannot divide by ZERO".PHP_EOL;
+	} else{
 	return $a % $b . PHP_EOL;
+	}
 }
 
-echo add(9, 3);
-echo subtract(9, 3);
-echo multiply(9, 3);
-echo divide(9, 3);
-echo modulus(9,3);
+
+
+
+
+echo add(6, 0);
+echo subtract(6, 0);
+echo multiply(6, 0);
+echo divide(6, 0);
+echo modulus(6,0);
