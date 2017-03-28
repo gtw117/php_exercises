4ae481bfaa2407fb54ad5abecc9ba1f735d24ad4
gtw117 <enigma@Enigmas-MacBook-Air.local>
2017-03-28 11:26:32 -0500

    Refactor code so that error messages are called locally within the funciton

--- arithmetic.php
+++ arithmetic.php
@@ -3,8 +3,25 @@
 $a = 6;
 $b = 0;
 
-if (!is_numeric($a) || !is_numeric($b)){
-	echo "ERROR: please use numbers only".PHP_EOL;
+
+// Validate all the arguments, and display an error if the input is not numeric.
+
+// Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+
+// Make the error messages show the values of the arguments.
+
+// Refactor the error messages into their own throwErrorMessage() function, have the other functions use it for error messaging.
+
+// 6.4.1
+
+function throwError($a, $b){
+	if (!is_numeric($a) || !is_numeric($b)){
+		echo "ERROR: $a or $b are not numeric, please use numbers only".PHP_EOL;
+	}
+
+	if ($b <= 0){
+		echo "cannot divide by $b".PHP_EOL;
+	}
 }
 
 function add($a, $b){
@@ -21,23 +38,19 @@ function multiply($a, $b){
 
 function divide($a, $b){
 	if ($b <= 0){
-		echo "cannot divide by ZERO".PHP_EOL;
-	} else{
-		return $as / $b .PHP_EOL;
+		return throwError($a, $b);
+	} else {
+	return $a / $b .PHP_EOL;
 	}
-	
-
 }
 
 function modulus($a, $b){
 	if ($b <= 0){
-		echo "cannot divide by ZERO".PHP_EOL;
-	} else{
-	return $a % $b . PHP_EOL;
+		return throwError($a, $b);
+	} else {
+	return $a / $b .PHP_EOL;
 	}
 }
-
-
 
 
 
